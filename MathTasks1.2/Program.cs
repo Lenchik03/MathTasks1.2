08a21f75fe4b0381fa404bebfa72d855f22560f2
Polchik <pyrina200052004@mail.ru>
2023-10-30 19:55:32 +1000

    Добавьте файлы проекта.

---
+++ MathTasks1.2/Program.cs
@@ -0,0 +1,137 @@
+﻿//задание 2
+//double x1, y1, x2, y2, x3, y3, P, S, a, b, c, p;
+
+//x1 = double.Parse(Console.ReadLine());
+//y1 = double.Parse(Console.ReadLine());
+//x2 = double.Parse(Console.ReadLine());
+//y2 = double.Parse(Console.ReadLine());
+//x3 = double.Parse(Console.ReadLine());
+//y3 = double.Parse(Console.ReadLine());
+
+//a = Math.Sqrt(Math.Pow(x2 - x1 , 2) + Math.Pow(y2 - y1, 2));
+//b = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+//c = Math.Sqrt(Math.Pow(x3 - x1, 2) + Math.Pow(y3 - y1, 2));
+
+//P = a + b + c;
+//p = P / 2;
+//S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+//Console.WriteLine("Периметр: " + P);
+//Console.WriteLine("Площадь:" + S);
+
+//задание 3
+//double R, P, S;
+//R = double.Parse(Console.ReadLine());
+
+//P = 2 * Math.PI * R;
+//S = Math.PI * Math.Pow(R, 2);
+
+//Console.WriteLine("Периметр: " + P);
+//Console.WriteLine("Площадь: " + S);
+
+// задание 5
+//double a, b, K, M;
+
+//a = double.Parse(Console.ReadLine());
+//b = double.Parse(Console.ReadLine());
+
+//K = ((a * a * a) + (b * b * b)) / 2;
+//M = Math.Abs(Math.Sqrt(a * b));
+
+//Console.WriteLine(K);
+//Console.WriteLine(M);
+
+//задание 6
+//double x1, y1, x2, y2, S;
+
+//x1 = double.Parse(Console.ReadLine());
+//y1 = double.Parse(Console.ReadLine());
+//x2 = double.Parse(Console.ReadLine());
+//y2 = double.Parse(Console.ReadLine());
+
+//S = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+
+//Console.WriteLine(S);
+
+//задание 7
+//double x, y, S, R, P, D;
+
+//x = double.Parse(Console.ReadLine());
+//y = double.Parse(Console.ReadLine());
+
+//S = x + y;
+//R = x - y;
+//P = x * y;
+//D = x / y;
+
+//Console.WriteLine(S);
+//Console.WriteLine(R);
+//Console.WriteLine(P);
+//Console.WriteLine(D);
+
+//задание 8
+//double r, Sg, Sp, V;
+
+//r = double.Parse(Console.ReadLine());
+
+//Sg = r * r;
+//Sp = 6 * Math.Pow(r, 2);
+//V = Math.Pow(r, 3);
+
+//Console.WriteLine(Sg);
+//Console.WriteLine(Sp);
+//Console.WriteLine(V);
+
+//задание 9
+//double d, S, h, R, r;
+
+//d = double.Parse(Console.ReadLine());
+
+//S = Math.Pow(d, 2) * Math.Sqrt(3) / 4;
+//h = d * Math.Sqrt(3) / 2;
+//r = d / 2*Math.Sqrt(3);
+//R = d / Math.Sqrt(3);
+
+//Console.WriteLine(S);
+//Console.WriteLine(h);
+//Console.WriteLine(r);
+//Console.WriteLine(R);
+
+// задание 10
+//double a, S;
+
+//a = double.Parse(Console.ReadLine());
+
+//S = Math.PI * Math.Pow(Math.PI, 2);
+//Console.WriteLine(S);
+
+// задание 11
+//double S, R, r;
+
+//r = double.Parse(Console.ReadLine());
+//R = double.Parse(Console.ReadLine());
+
+//S = Math.PI * (Math.Pow(R, 2) - Math.Pow(r, 2));
+
+//Console.WriteLine(S);
+
+// задание 12
+//double a, b, c, R;
+
+//a = double.Parse(Console.ReadLine());
+//b = double.Parse(Console.ReadLine());
+//c = double.Parse(Console.ReadLine());
+//R = double.Parse(Console.ReadLine());
+
+//a = 2 * R * Math.Sin(a * Math.PI / 180);
+//b = 2 * R * Math.Sin(a * Math.PI / 180);
+//c = 2 * R * Math.Sin(a * Math.PI / 180);
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+
+
+
+
+
